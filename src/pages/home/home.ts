6df5a6b6987dff609e6eb453c51fbe16969619a2
Vish Patel <vishpatel913@googlemail.com>
2017-12-23 15:20:00 +0000

    base buttons set up with inputs and alerts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,55 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  rent = 452.40;
+  snaked: string = "something";
+  user = "Vish";
+
+  constructor(
+    public navCtrl: NavController,
+    private alertCtrl: AlertController
+  ) {
 
   }
+
+  setRent(event) {
+    if (event.target.value.length > 1) {
+      this.rent = event.target.value;
+    }
+  }
+
+  setSnaked(event) {
+    if (event.target.value.length > 2) {
+      this.snaked = event.target.value;
+    }
+  }
+
+  sendRent() {
+    let title = 'Rent Due';
+    let subtitle = `Amount due this month: £${Number(this.rent).toFixed(2)}`;
+    this.notiAlert(title, subtitle);
+  }
+
+  sendSnaked() {
+    let title = `Snaked Item`;
+    let subtitle = `${this.user} has eaten/taken <b>${this.snaked}</b>, don't worry it'll be replaced.`;
+    this.notiAlert(title, subtitle);
+  }
+
+  sendBins() {
+    let title = `Bins`;
+    let subtitle = `Bins need taking out`;
+    this.notiAlert(title, subtitle);
+  }
+
+  notiAlert(title, subtitle) {
+    let alert = this.alertCtrl.create({
+      title: title,
+      subTitle: subtitle,
+      buttons: ['Okay']
+    });
+    alert.present();
+  }
+
 
 }
